9d9becc00572220f77ccb0988982855cb5cba855
kevinwang <kevinwang5658@gmail.com>
2026-01-03 12:50:21 -0500

    feat: Added build script to bin

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'cjs',
+    inlineDynamicImports: true,
+  },
+  external: ['@homebridge/node-pty-prebuilt-multiarch'],
+  plugins: [
+    json(),
+    nodeResolve({ exportConditions: ['node'] }),
+    typescript({
+      exclude: ['**/*.test.ts', '**/*.d.ts', 'test']
+    }),
+    commonjs(),
+    terser()
+  ]
+}
